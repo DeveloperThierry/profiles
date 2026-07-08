0c766af5f5b6c8837717aad7a35aff2d4dc14cf5
developerthierry <developerthierry@gmail.com>
2026-07-08 20:06:41 +0000

    Implement CreateProfie, UpdateProfile, GetProfile

---
+++ src/profiles/profiles.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
+
+@Controller('profiles')
+export class ProfilesController {
+  @Get()
+  findAll() {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return {id};
+  }
+
+  @Post()
+  create(@Body() createProfileDto: CreateProfileDto) {
+    return {
+      name: createProfileDto.name,
+      description: createProfileDto.description,
+    };
+  }
+
+  @Put(':id')
+  update(@Param('id') id:string, @Body() updateProfileDto:UpdateProfileDto){
+    return {id, name: updateProfileDto.name,
+        description: updateProfileDto.description,}
+  }
+}
